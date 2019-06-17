7ced955d1c00c4098975c4eff0a7a60034754506
CapitainePigeon <lucas69300@outlook.fr>
2019-06-17 10:22:55 +0200

    footer

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,10 +8,10 @@
 </head>
 <body>
 
-<nav class="d-block navbar navbar-expand-lg grey-text static-top" style= "background-color: #c1bebe; color: #fa4040 ">
+<nav class="d-block navbar navbar-expand-lg grey-text static-top" style= "background-color: #eee; color: #fa4040 ">
     <div class="container-fluid">
 
-        <a class="navbar-brand" style="width:30%; min-width: 300px;" href="{{url('/home')}}" ><img src="Images/ImageCoordonnee/logo_transparent.png" class="img-fluid rounded" alt="Logo manquant"></a>
+        <a class="navbar-brand" style="width:30%; min-width: 300px;" href="{{url('/home')}}" ><img src="Images/ImageCoordonnee/logo_ombre_transparent.png" class="img-fluid rounded" alt="Logo manquant"></a>
         <button class="navbar-toggler" type="button" style="width:30%; max-width: 100px;" data-toggle="collapse" data-target="#LimpiluxNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span><img src="Images/hamburger_menu.png" class="img-fluid float-right"  alt="Menu"></span>
         </button>
@@ -65,7 +65,7 @@
 
 @yield('contenu')
 
-<footer class="footer mt-5">
+<footer class="footer mt-5" style= "background-color: #eee;">
     <p>Tel : 0820 205 122</p>
     <p>Copyright © Limpilux 2019 - Tous droits réservés</p>
 </footer>
