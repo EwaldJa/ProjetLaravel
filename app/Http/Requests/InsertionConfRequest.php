49a0185ad365157f88b2e87648bb1f0829af7fd6
EwaldJa <ewald.janin@users.noreply.github.com>
2019-06-17 09:20:30 +0200

    Started working on editing

--- app/Http/Requests/InsertionConfRequest.php
+++ app/Http/Requests/InsertionConfRequest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Requests;
+use Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -13,7 +14,10 @@ class InsertionConfRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        if (Auth::user() != null) {
+            return true;
+        }
+        return false;
     }
 
     /**
